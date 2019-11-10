7d24e2f6a6c81ed4b87e133303e40868c1d473dd
Matteo Pierro <pierromatteo@gmail.com>
2019-11-10 11:49:40 +0100

    add empty AcceptanceTest

---
+++ src/test/java/it/matteopierro/bankaccount/feature/PrintStatementAcceptanceTest.java
@@ -0,0 +1,11 @@
+package it.matteopierro.bankaccount.feature;
+
+import org.junit.jupiter.api.Test;
+
+public class PrintStatementAcceptanceTest {
+
+    @Test
+    void xxx() {
+        throw new RuntimeException("START WITH AN ACCEPTANCE TEST!!!");
+    }
+}
