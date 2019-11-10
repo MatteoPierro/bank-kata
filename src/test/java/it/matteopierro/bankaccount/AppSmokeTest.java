cd9439d877be59c376e04bbe1b6c396cbd9bc829
Matteo Pierro <pierromatteo@gmail.com>
2019-11-10 11:52:38 +0100

    add smoke test

---
+++ src/test/java/it/matteopierro/bankaccount/AppSmokeTest.java
@@ -0,0 +1,11 @@
+package it.matteopierro.bankaccount;
+
+import org.junit.jupiter.api.Test;
+
+class AppSmokeTest {
+
+    @Test
+    void smoke_test() {
+        App.main(new String[]{});
+    }
+}
