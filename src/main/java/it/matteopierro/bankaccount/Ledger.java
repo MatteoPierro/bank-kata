bfcbfb2975244cf146532634e4fcdddded0c5e8b
Matteo Pierro <pierromatteo@gmail.com>
2019-11-15 11:16:52 +0100

    Adding a test for the ledger

---
+++ src/main/java/it/matteopierro/bankaccount/Ledger.java
@@ -0,0 +1,7 @@
+package it.matteopierro.bankaccount;
+
+public class Ledger {
+    public void addDeposit(int amount) {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ src/test/java/it/matteopierro/bankaccount/AccountTest.java
@@ -0,0 +1,24 @@
+package it.matteopierro.bankaccount;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class AccountTest {
+
+    @Mock
+    Ledger ledger;
+
+    @Test
+    void testDeposit() {
+        Account account = new Account(ledger);
+
+        account.deposit(1);
+
+        verify(ledger).addDeposit(1);
+    }
+}
